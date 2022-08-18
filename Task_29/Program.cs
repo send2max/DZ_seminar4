302427f07d7fc39fcf80febe3403ccaebd5bb10e
максим <send2max@bk.ru>
2022-08-18 09:46:03 +0300

    Решена первая задача

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу,
+//  которая задаёт массив из 8 элементов 
+//  и выводит их на экран.
+//  1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Write("Введите колчисетво элементов массива: ");
+int number = int.Parse(Console.ReadLine());
+int[] arr = new int[number];
+
+void FillArray(int num, int[] array)
+{
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(1,99); 
+    }
+}
+
+void PrintArray(int num_2, int[] array_2)
+{
+    Console.Write($"array = [ ");
+    for (int i = 0; i < num_2; i++)
+    {
+        Console.Write($"{array_2[i]} ");
+    }
+    Console.Write($"]");
+}
+
+
+FillArray(number, arr);
+PrintArray(number, arr);
